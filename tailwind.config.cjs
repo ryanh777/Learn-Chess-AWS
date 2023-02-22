7508164161b24c1815d3d60491f36615362df6bf
Ryan Humphrey <ryanhumphrey777@gmail.com>
2023-02-22 01:03:06 -0500

    tailwindcss installed

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+     extend: {
+        colors: {
+           bgprimary: "#312e2b",
+           bgsecondary: "#272522",
+           bgtertiary: "#21201d",
+           button: "#52514e",
+           buttonHover: "#83817c",
+           textprimary: "#d4d3d3",
+        },
+     },
+  },
+  plugins: [],
+};
