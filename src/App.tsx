52800ce229d1f07bb111289ddf0d2b4c1814e3c8
Ryan Humphrey <ryanhumphrey777@gmail.com>
2023-03-10 14:45:25 -0500

    user data stored in move

--- src/App.tsx
+++ src/App.tsx
@@ -8,154 +8,154 @@ import { resetAndSetWhiteRootMove, resetAndSetBlackRootMove, setPrevMoveToRoot }
 import { setUser } from './redux/slices/user';
 
 function App() {
-	const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
-    const dispatch = useAppDispatch();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+   const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
+   const dispatch = useAppDispatch();
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+   const [error, setError] = useState("");
 
-    useEffect(() => {
-		const token = localStorage.getItem('token')
-		if (!token) return
-		const fetchToken = async () => {
-			const response = await fetch('/api/user', {
-				method: 'GET',
-				headers: {
-					'auth-token': `${token}`
-				}
-			})
-			if (response.ok) {
-                dispatchLogin(await response.json());
-			}
-		}
-		fetchToken()
-	}, [])
-
-    const register = async () => {
-        try {
-            const response = await fetch('api/user/exists', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username: username})
-            })
-            if (!response.ok) {
-                throw new Error(await response.text())
-            } 
-        } catch (err: any) {
-            console.log(err)
-            setError(err.message)
-            return
-        }
-
-        const whiteRoot = {
-            move: `${username}-white-root`,
-            parentID: "root",
-            piece: "root"
-        }
-        const blackRoot = {
-            move: `${username}-black-root`,
-            parentID: "root",
-            piece: "root"
-        }
-        const user = {
-            username: username,
-            password: password,
-            whiteRootID: await postMove(whiteRoot),
-            blackRootID: await postMove(blackRoot)
-        }
-        try {
-            const response = await fetch('/api/user/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-            })
-            if (!response.ok) {
-                throw new Error(await response.text())
-            } 
-            dispatchLogin({
-                username: username,
-                whiteRootID: user.whiteRootID,
-                blackRootID: user.blackRootID
-            })
-        } catch (err: any) {
-            console.log(err)
-            setError(err)
-        }
-    };
-
-    const login = async () => {
-        const user = {
-			username: username,
-			password: password
-		}
-        try {
-            const response = await fetch('/api/user/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-            })
-      
-            if (!response.ok) throw new Error(await response.text())
-      
-            const token: string | null = response.headers.get('auth-token')
-            if (!token) throw new Error("token returned null")
-            localStorage.setItem('token', token)
-
-            const filteredResponse = await response.json()
-            dispatchLogin({
-                username: username,
-                whiteRootID: filteredResponse.whiteRootID,
-                blackRootID: filteredResponse.blackRootID,
-            })
-        } catch (err: any) {
-            console.log(err.message)
-            setError(err.message)
-        }
-    }
-
-    const dispatchLogin = async (
-        user: User
-    ) => {
-        const whiteRootMove: Move = await fetchMove(user.whiteRootID);
-        const blackRootMove: Move = await fetchMove(user.blackRootID);
-        dispatch(resetAndSetWhiteRootMove(whiteRootMove));
-        dispatch(resetAndSetBlackRootMove(blackRootMove));
-        dispatch(setPrevMoveToRoot())
-        dispatch(setUser(user));
-    }
-	
-	return (
-        <div>
-			{ isLoggedIn ?
-                <div className='flex items-center min-h-screen bg-bgprimary text-textprimary'>
-                    <MainContent/>
-                    <Sidebar/>
-                </div>
-                :
-                <div>
-                    <div>
-                        Register:
-                        <input placeholder='username' onChange={(e) => setUsername(e.target.value)}></input>
-                        <input placeholder='password' onChange={(e) => setPassword(e.target.value)}></input>
-                        <button onClick={register}>yessir</button>
-                    </div>
-                    <div>
-                        Login:
-                        <input placeholder='username' onChange={(e) => setUsername(e.target.value)}></input>
-                        <input placeholder='password' onChange={(e) => setPassword(e.target.value)}></input>
-                        <button onClick={login}>yessir</button>
-                    </div>
-                    <div className='text-red-500'>{error}</div>
-                </div>
+   useEffect(() => {
+      const token = localStorage.getItem('token')
+      if (!token) return
+      const fetchToken = async () => {
+         const response = await fetch('/api/user', {
+            method: 'GET',
+            headers: {
+               'auth-token': `${token}`
             }
-		</div>
-	);
+         })
+         if (response.ok) {
+            dispatchLogin(await response.json());
+         }
+      }
+      fetchToken()
+   }, [])
+
+   const register = async () => {
+      try {
+         const response = await fetch('api/user/exists', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: username })
+         })
+         if (!response.ok) {
+            throw new Error(await response.text())
+         }
+      } catch (err: any) {
+         console.log(err)
+         setError(err.message)
+         return
+      }
+
+      const whiteRoot = {
+         user: `${username}`,
+         move: "white-root",
+         piece: "root"
+      }
+      const blackRoot = {
+         user: `${username}`,
+         move: "black-root",
+         piece: "root"
+      }
+      const user = {
+         username: username,
+         password: password,
+         whiteRootID: await postMove(whiteRoot),
+         blackRootID: await postMove(blackRoot)
+      }
+      try {
+         const response = await fetch('/api/user/register', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+         })
+         if (!response.ok) {
+            throw new Error(await response.text())
+         }
+         dispatchLogin({
+            username: username,
+            whiteRootID: user.whiteRootID,
+            blackRootID: user.blackRootID
+         })
+      } catch (err: any) {
+         console.log(err)
+         setError(err)
+      }
+   };
+
+   const login = async () => {
+      const user = {
+         username: username,
+         password: password
+      }
+      try {
+         const response = await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+         })
+
+         if (!response.ok) throw new Error(await response.text())
+
+         const token: string | null = response.headers.get('auth-token')
+         if (!token) throw new Error("token returned null")
+         localStorage.setItem('token', token)
+
+         const filteredResponse = await response.json()
+         dispatchLogin({
+            username: username,
+            whiteRootID: filteredResponse.whiteRootID,
+            blackRootID: filteredResponse.blackRootID,
+         })
+      } catch (err: any) {
+         console.log(err.message)
+         setError(err.message)
+      }
+   }
+
+   const dispatchLogin = async (
+      user: User
+   ) => {
+      const whiteRootMove: Move = await fetchMove(user.whiteRootID);
+      const blackRootMove: Move = await fetchMove(user.blackRootID);
+      dispatch(resetAndSetWhiteRootMove(whiteRootMove));
+      dispatch(resetAndSetBlackRootMove(blackRootMove));
+      dispatch(setPrevMoveToRoot())
+      dispatch(setUser(user));
+   }
+
+   return (
+      <div>
+         {isLoggedIn ?
+            <div className='flex items-center min-h-screen bg-bgprimary text-textprimary'>
+               <MainContent />
+               <Sidebar />
+            </div>
+            :
+            <div>
+               <div>
+                  Register:
+                  <input placeholder='username' onChange={(e) => setUsername(e.target.value)}></input>
+                  <input placeholder='password' onChange={(e) => setPassword(e.target.value)}></input>
+                  <button onClick={register}>yessir</button>
+               </div>
+               <div>
+                  Login:
+                  <input placeholder='username' onChange={(e) => setUsername(e.target.value)}></input>
+                  <input placeholder='password' onChange={(e) => setPassword(e.target.value)}></input>
+                  <button onClick={login}>yessir</button>
+               </div>
+               <div className='text-red-500'>{error}</div>
+            </div>
+         }
+      </div>
+   );
 }
 
 export default App;
--- src/components/saveButton.tsx
+++ src/components/saveButton.tsx
@@ -5,76 +5,77 @@ import { useAppDispatch, useAppSelector } from '../redux/hooks'
 import { resetAndSetBlackRootMove, resetAndSetWhiteRootMove, setPrevMoveToRoot } from '../redux/slices/board'
 
 const SaveButton = (): JSX.Element => {
-    const dispatch = useAppDispatch();
-    const moves = useAppSelector((state) => state.board.moveData);
-    const boardOrientation = useAppSelector((state) => state.board.boardOrientation);
-    const user = useAppSelector((state) => state.user);
+   const dispatch = useAppDispatch();
+   const moves = useAppSelector((state) => state.board.moveData);
+   const boardOrientation = useAppSelector((state) => state.board.boardOrientation);
+   const user = useAppSelector((state) => state.user);
 
-    const createChild = async (parentID: string, move: string, piece: string): Promise<string> => {
-		const newChild = {
-			move: move,
-            piece: piece
-		}
-		const id = await postMove(newChild)
-		const childInfo: MoveData = {
-			id: id,
-			move: move,
-            piece: piece
-		}
-		await fetch(`/api/data/add/${parentID}`, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(childInfo)
-		})
-			.then(res => res.json())
-			.then(data => console.log("data:", data))
-		return id;
-	}
+   const createChild = async (parentID: string, move: string, piece: string): Promise<string> => {
+      const newChild = {
+         user: user.username,
+         move: move,
+         piece: piece
+      }
+      const id = await postMove(newChild)
+      const childInfo: MoveData = {
+         id: id,
+         move: move,
+         piece: piece
+      }
+      await fetch(`/api/data/add/${parentID}`, {
+         method: 'PATCH',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(childInfo)
+      })
+         .then(res => res.json())
+         .then(data => console.log("data:", data))
+      return id;
+   }
 
-    const handleClick = async () => {
-        if (moves.length === 0) {
-            console.log("must make a move before saving")
-            return
-        }
+   const handleClick = async () => {
+      if (moves.length === 0) {
+         console.log("must make a move before saving")
+         return
+      }
 
-        let id: string = boardOrientation === "white" ? user.whiteRootID : user.blackRootID
-        let childData: MoveData[] = await getChildren(id)
+      let id: string = boardOrientation === "white" ? user.whiteRootID : user.blackRootID
+      let childData: MoveData[] = await getChildren(id)
 
-        for (let i = 0; i < moves.length; i++) {
-            let hasChildren: boolean = (childData.length > 0) ? true : false
-            if (hasChildren) {
-                let broken: boolean = false
-                for (let j = 0; j < childData.length; j++) {
-                    if (childData[j].move === moves[i].move) {
-                        id = childData[j].id
-                        childData = await getChildren(id)
-                        broken = true
-                        break
-                    }
-                }
-                if (!broken) id = await createChild(id, moves[i].move, moves[i].piece)
-            } else {
-                id = await createChild(id, moves[i].move, moves[i].piece)
+      for (let i = 0; i < moves.length; i++) {
+         let hasChildren: boolean = (childData.length > 0) ? true : false
+         if (hasChildren) {
+            let broken: boolean = false
+            for (let j = 0; j < childData.length; j++) {
+               if (childData[j].move === moves[i].move) {
+                  id = childData[j].id
+                  childData = await getChildren(id)
+                  broken = true
+                  break
+               }
             }
-        }
-        const rootMove: Move = await getRootMove(boardOrientation, user);
-        boardOrientation == Orientation.white ? 
-            dispatch(resetAndSetWhiteRootMove(rootMove))
-        :  
-            dispatch(resetAndSetBlackRootMove(rootMove))
-        dispatch(setPrevMoveToRoot())
-    }
+            if (!broken) id = await createChild(id, moves[i].move, moves[i].piece)
+         } else {
+            id = await createChild(id, moves[i].move, moves[i].piece)
+         }
+      }
+      const rootMove: Move = await getRootMove(boardOrientation, user);
+      boardOrientation == Orientation.white ?
+         dispatch(resetAndSetWhiteRootMove(rootMove))
+         :
+         dispatch(resetAndSetBlackRootMove(rootMove))
+      dispatch(setPrevMoveToRoot())
+   }
 
-    return (
-        <button 
-            className="flex items-center justify-center flex-grow mr-1 text-lg bg-button rounded-xl hover:bg-buttonHover"
-            onClick={handleClick}
-            >
-            {<RiSave3Fill size={36}/>}
-        </button>
-    )
-}   
+   return (
+      <button
+         className="flex items-center justify-center flex-grow mr-1 text-lg bg-button rounded-xl hover:bg-buttonHover"
+         onClick={handleClick}
+      >
+         {<RiSave3Fill size={36} />}
+      </button>
+   )
+}
 
 export default SaveButton;
